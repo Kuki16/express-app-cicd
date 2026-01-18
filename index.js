80350eccc7cc593915299bd713d5aa7c4326d4fb
Kuki16 <amelialipkowska@gmail.com>
2026-01-18 23:32:28 +0100

    Aktualizacja tekstu

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Wersja 1')
+  res.send('Hello World! Wersja 2')
 })
 
 app.listen(8080, () => {
